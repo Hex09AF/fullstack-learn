ba6cbe0ec71232ebb0c38dec90f36d167f368d51
Hex09AF <lsqhuy@gmail.com>
2023-04-13 04:10:14 +0700

    :bugs: fix bugs tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,10 @@ const config = {
 		extend: {},
 	},
 
-	plugins: [require('@skeletonlabs/skeleton/tailwind/skeleton.cjs'), require('@tailwindcss/forms')],
+	plugins: [
+		require('@tailwindcss/forms'),
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')(),
+	],
 };
 
 module.exports = config;
