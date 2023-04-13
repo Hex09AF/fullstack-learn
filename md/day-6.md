d2a24e339726e5f3f8ff88b72b5b293d34faa8b8
Hex09AF <lsqhuy@gmail.com>
2023-04-14 01:10:32 +0700

    ✨ add oauth flow

---
+++ md/day-6.md
@@ -0,0 +1,8 @@
+# Day 6: Github Actions | Auth.js +SvelteKit | City Guessr | Day 6
+
+1. [actions: lint on PRs](https://github.com/CodingGarden/listd/commit/f29e394de7735b1d8abe3f92d7944ac22f3d389e)
+
+Note:
+
+- upgrade dependency https://youtu.be/3y1RKPAhVU4?t=7008
+  - npx npm-upgrade
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	import { signIn } from '@auth/sveltekit/client';
 	import { LL } from '$lib/i18n/i18n-svelte';
+	// TODO: fix $app import error with eslint
+	// eslint-disable-next-line import/no-unresolved
+	import { page } from '$app/stores';
 </script>
 
 <div class="hero-container flex flex-col items-center justify-center p-4">
 	<p class="my-4 text-center">{$LL.tagline()}</p>
-	<a href="/onboarding" class="btn variant-filled-primary" data-sveltekit-preload-data="hover">
-		{$LL.signUp()}
-	</a>
+	{#if $page.data.session}
+		<p>{$page.data.session.user?.name ?? 'User'} logged in</p>
+	{:else}
+		<button on:click={() => signIn('google')} class="btn variant-filled-primary cursor-pointer">
+			{$LL.signUp()}
+		</button>
+	{/if}
 </div>
